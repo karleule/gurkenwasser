0ec4c76e4b7bb64631a2321a432be124ce3ebab3
Daniel Schulz <karleule@posteo.de>
2018-02-14 02:43:40 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app>
+        <v-toolbar>
+            <div>
+                Welcome to Gurkenwasser
+            </div>
+        </v-toolbar>
+        <v-content>
+            <v-container fill-height>
+                <v-layout justify-center align-center>
+                    Welcome to Gurkenwasser!
+                </v-layout>
+            </v-container>
+        </v-content>
+        <v-footer :fixed="fixed" app>
+            <span>&copy; 2017</span>
+        </v-footer>
+    </v-app>
+</template>
+
+<script>
+export default {
+    data () {
+      return {
+        title: 'Gurkenwasser',
+        fixed: false
+      }
+    }
+}
+</script>
